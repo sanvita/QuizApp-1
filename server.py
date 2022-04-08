d030ef0f3d0c5861023b779bdb202f072ede1639
sanvita <100260161+sanvita@users.noreply.github.com>
2022-04-08 09:15:59 +0300

    jea
    te viss kautkas

--- server.py
+++ server.py
@@ -4,6 +4,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+def loadAllPictures():
+  filepath = 'picDB.txt'
+  pictures=[]
+  with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!='':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return pictures
+
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
@@ -18,7 +33,8 @@ def about():
 @app.route('/tets',methods = ['POST', 'GET'])
 def tets():
     parametri=["IQ","Augums","Kājas izmērs"]
-    return render_template("tets.html", parametri=parametri)
+    images=loadAllPictures()
+    return render_template("tets.html", parametri=parametri, images=images)
     
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
